2f04d109be0626dfaeee472b1cf85af1327e20e6
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-05-29 08:30:39 -0500

    udpating server.js to server client in heroku

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 // Static directory
 app.use(express.static("public"));
 
