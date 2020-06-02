1bcb0dda4b0c50b64b903d16279f06b5f1ca4c69
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-06-02 12:00:24 -0500

    temp change to create worker

--- client/src/pages/ClientCreateLogin.js
+++ client/src/pages/ClientCreateLogin.js
@@ -26,7 +26,7 @@ function ClientCreateLogin() {
             API.createLoginCredentials({
                 email: username,
                 password: password,
-                userType: "C"
+                userType: "W"
             })
             .then(res => {
                 // Re-direct to page where client can enter name, address, and other info
